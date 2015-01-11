776bfb28ab196313632ce2d453e778ab6bd582d0
Hung Tran <oohnoitz@gmail.com>
2015-01-11 04:30:51 -0600

    add config setting to unlink or rename file

--- client/client.js
+++ client/client.js
@@ -2,14 +2,24 @@ var fs = require('fs'),
   redis = require('redis'),
   client;
 
-var processFile = function (path, file) {
-  fs.rename(path + file, path + file + '.banned', function (err) {
-    if (err) {
-      console.log("\u2717 " + path + file);
-    } else {
-      console.log("\u2713 " + path + file);
-    }
-  });
+var processFile = function (purge, path, file) {
+  if (purge === true) {
+    fs.unlink(path + file, function (err) {
+      if (err) {
+        console.log("\u2717 " + path + file);
+      } else {
+        console.log("\u2713 " + path + file);
+      }
+    });
+  } else {
+    fs.rename(path + file, path + file + '.banned', function (err) {
+      if (err) {
+        console.log("\u2717 " + path + file);
+      } else {
+        console.log("\u2713 " + path + file);
+      }
+    });
+  }
 }
 
 fs.readFile('./config.json', { encoding: 'utf8' }, function (err, config) {
@@ -23,7 +33,7 @@ fs.readFile('./config.json', { encoding: 'utf8' }, function (err, config) {
   client.subscribe('foolfuuka:plugin:nas-media-purge');
   client.on('message', function (channel, file) {
     for (var i = 0, len = config.paths.length; i < len; i++) {
-      processFile(config.paths[i], file);
+      processFile(config.purge, config.paths[i], file);
     }
   });
 });
